ac7f1d1cb77da425546f7ea2c763d09fa78fa528
Nader Mohamed <134107835+Nadercr7@users.noreply.github.com>
2024-10-18 16:59:41 +0300

    Update Math_Functions.py

--- Math_Functions.py
+++ Math_Functions.py
@@ -16,3 +16,16 @@ def abs_max(x,y) :
         x = abs(x)
         y = abs(y)
         if x != y : return x if x > y else y
+
+
+def floor(x):
+    if x == int(x):   return int(x)
+    elif x > 0:       return int(x)
+    else:             return int(x) - 1
+
+
+def ceil(x):
+    if x == int(x):  return int(x)
+    elif x > 0:      return int(x) + 1
+    else:            return int(x)
+
